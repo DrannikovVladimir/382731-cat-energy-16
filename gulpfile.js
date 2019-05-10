3109339080e960b5aad5e2e8be30aff4f4bd72aa
Vladimir Drannikov <krowen@inbox.ru>
2019-05-10 15:10:12 +0600

    Завершил автоматическую сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var server = require("browser-sync").create();
 var webp = require("gulp-webp");
 var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -26,18 +27,16 @@ gulp.task("css", function () {
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
-
-gulp.task("posthtml", function () {
+gulp.task("html", function () {
   return gulp.src("source/*.html")
-    .pipe(posthtml({
+    .pipe(posthtml([
       include()
-    }))
-    .pipe(gulp.dest("source"));
+    ]))
+    .pipe(gulp.dest("build"));
 });
-
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -47,25 +46,22 @@ gulp.task("images", function () {
     ]))
     .pipe(gulp.dest("source/img"));
 });
-
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 80}))
     .pipe(gulp.dest("source/img"));
 });
-
 gulp.task("sprite", function () {
   return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"))
 });
-
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -73,7 +69,27 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+gulp.task("clean", function () {
+  return del("build");
+});
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+})
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("picture", gulp.series("images", "webp"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
